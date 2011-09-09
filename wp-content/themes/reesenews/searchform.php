4051e5fa0cf49050e122bc50d07d4c65b4d192d6
Zach Dexter <zach@databracket.com>
2011-09-08 23:58:29 -0400

    Remove theme to make way for submodule

--- wp-content/themes/reesenews/searchform.php
+++
@@ -1,4 +0,0 @@
-<form role="search" method="get" id="searchform" action="<?php bloginfo('url'); ?>">
-        <input type="text" value="" name="s" id="s" />
-        <input type="submit" id="searchsubmit" value="Search" />
-</form>
